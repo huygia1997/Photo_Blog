b3f86e57ef483661846d6c0c27b60af923b14916
huygia1997 <huygia1997@.com>
2018-02-06 13:54:26 +0700

    finish static page

--- Gemfile
+++ Gemfile
@@ -6,10 +6,20 @@ git_source(:github) do |repo_name|
 end
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
+gem 'jquery-rails'
 gem 'rails', '~> 5.1.4'
-# Use postgresql as the database for Active Record
-gem 'pg', '~> 0.18'
+gem 'bcrypt', '3.1.11'
+gem 'faker',  '1.7.3'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
+
+# add bootstrap
+gem 'bootstrap-sass', '3.3.7'
+
+gem 'pg',  '0.20.0'
+gem 'fog', '1.42'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,60 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+	// menu
+    $('#drop_trigger').mouseenter(function(){
+							    $('#drop_show').show(300);
+							});
+	$('#drop_show').mouseleave(function(){
+	    $('#drop_show').hide(300);
+	});
+
+	// back to top
+	$(window).scroll(function () {
+            if ($(this).scrollTop() > 150) {
+                $('#back_to_top').fadeIn();
+            } else {
+                $('#back_to_top').fadeOut();
+            }
+
+            if ($(this).scrollTop() > 300) {
+                $('#login_div').animate({
+                    left: '30%'
+                }, 500).show(500);
+
+            }
+
+            if ($(this).scrollTop() > 800) {
+                $('#user_div').animate({
+                    right: '30%'
+                }, 500).show(500);
+
+            }
+
+            if ($(this).scrollTop() > 1300) {
+                $('#about_div').animate({
+                    left: '30%'
+                }, 500).show(500);
+
+            }
+        });
+        // scroll body to 0px on click
+        $('#back_to_top').click(function () {
+            $('#back_to_top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back_to_top').tooltip('show');
+
+});
